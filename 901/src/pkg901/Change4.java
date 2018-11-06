a09ad9f85d756be071d007a6ff7f12345c41c63c
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-06 12:35:37 -0800

    Update 901/src/pkg901/Change4.java

--- 901/src/pkg901/Change4.java
+++ 901/src/pkg901/Change4.java
@@ -4,7 +4,7 @@ package pkg901;
 public class Change4 {
     //Change more5
     //Stalin Comrade
-    //Boys, Run it, Commit suicide, and push me to the edge all my friends are dead
+    //Boys, Run i
 
 
 }
