f5220ab9644076d18df27286930dfd7f84f8490d
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-06 12:39:11 -0800

    Update 901/src/pkg901/Main.java

--- 901/src/pkg901/Main.java
+++ 901/src/pkg901/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     
     public static void main(String[] args) {
-        //shedadle suduction, seze thy means of production
+        //shedadle suduction, seze thy means of 
     }
     
 }
