8ac3b9cedfbb787ad3f29b163271dc31239d54fc
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-08 12:44:19 -0800

    Update 901/src/pkg901/Room901.java

--- 901/src/pkg901/Room901.java
+++ 901/src/pkg901/Room901.java
@@ -155,28 +155,8 @@ import java.util.ArrayList;
         
         g.drawImage(Student,Window.getX(440),Window.getY(400),
                 Window.getX(220),Window.getY(260),this);
-
-
 =======
         
-        g.drawImage(Student,Window.getX(440),Window.getY(500),
-                Window.getX(220),Window.getY(260),this);
-        
-        g.drawImage(Blank,Window.getX(1240),Window.getY(500),
-                Window.getX(220),Window.getY(260),this);
-         g.drawImage(Blank,Window.getX(150),Window.getY(400),
-                Window.getX(200),Window.getY(100),this);
-
-        
-        
-        
-        
-
-
-
-
-
->>>>>>> bdd41ce84f57553c6dd43dfea6b16ed3dc887b58
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
 
