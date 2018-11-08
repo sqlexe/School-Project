c5874ca761715356d8ce107ac1029a798236e13d
blackblackadder <44787656+blackblackadder@users.noreply.github.com>
2018-11-08 12:45:33 -0800

    Update 901/src/pkg901/Room901.java

--- 901/src/pkg901/Room901.java
+++ 901/src/pkg901/Room901.java
@@ -149,13 +149,13 @@ import java.util.ArrayList;
         g.drawImage(Tamperer,Window.getX(1240),Window.getY(50),
                 Window.getX(220),Window.getY(260),this);
 
-<<<<<<< HEAD
+
         g.drawImage(Blank,Window.getX(1240),Window.getY(400),
                    Window.getX(220),Window.getY(260),this);
         
         g.drawImage(Student,Window.getX(440),Window.getY(400),
                 Window.getX(220),Window.getY(260),this);
-=======
+
         
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
