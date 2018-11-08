eaecd82a511f0aea5db496e59b11e14bcfba77b7
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-08 11:34:52 -0800

    Update 901/src/pkg901/SpaceShip.java

--- 901/src/pkg901/SpaceShip.java
+++ 901/src/pkg901/SpaceShip.java
@@ -136,23 +136,20 @@ import java.util.ArrayList;
 
         g.drawImage(FeltBG,Window.getX(0),Window.getY(0),
                 Window.getWidth2(),Window.getHeight2(),this);
-        
-<<<<<<< HEAD
-        g.drawImage(Middle,Window.getX(80),Window.getY(80),
-                Window.getX(160),Window.getY(160),this);
+
         
         g.drawImage(MrYee,Window.getX(200),Window.getY(160),
-                Window.getX(180),Window.getY(200),this);
+                Window.getX(220),Window.getY(260),this);
         
         g.drawImage(Alex,Window.getX(100),Window.getY(100),
                 Window.getX(160),Window.getY(160),this);
         
         g.drawImage(Tamperer,Window.getX(120),Window.getY(120),
                 Window.getX(160),Window.getY(160),this);
-=======
+
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
->>>>>>> 871b76787123ce88ab3debea72ccf3dcc79c8322
+
 
     
         gOld.drawImage(image, 0, 0, null);
