920a36ad3303c9e2376e2803c64cd278781e4877
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-08 11:42:17 -0800

    Update 901/src/pkg901/SpaceShip.java

--- 901/src/pkg901/SpaceShip.java
+++ 901/src/pkg901/SpaceShip.java
@@ -136,31 +136,15 @@ import java.util.ArrayList;
 
         g.drawImage(FeltBG,Window.getX(0),Window.getY(0),
                 Window.getWidth2(),Window.getHeight2(),this);
-<<<<<<< HEAD
-
-=======
-        
-
-
-        g.drawImage(Middle,Window.getX(80),Window.getY(80),
-                Window.getX(160),Window.getY(160),this);
-
-//        g.drawImage(Middle,Window.getX(80),Window.getY(80),
-//                Window.getX(160),Window.getY(160),this);
-
-
-//        g.drawImage(Middle,Window.getX(80),Window.getY(80),
-//                Window.getX(160),Window.getY(160),this);
->>>>>>> 3a352fbfccb5874ad0fca9ddad805f6ee26b0b73
-        
+   
         g.drawImage(MrYee,Window.getX(200),Window.getY(160),
                 Window.getX(220),Window.getY(260),this);
         
-        g.drawImage(Alex,Window.getX(100),Window.getY(100),
-                Window.getX(160),Window.getY(160),this);
+        g.drawImage(Alex,Window.getX(300),Window.getY(100),
+                Window.getX(220),Window.getY(260),this);
         
-        g.drawImage(Tamperer,Window.getX(120),Window.getY(120),
-                Window.getX(160),Window.getY(160),this);
+        g.drawImage(Tamperer,Window.getX(400),Window.getY(120),
+                Window.getX(220),Window.getY(260),this);
 
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
