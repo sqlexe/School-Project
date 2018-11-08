39c6f2a232310d75bc418bf18974c2f938d8a39d
Don't Bother <clj08222@gmail.com>
2018-11-08 11:57:22 -0800

    Revert "Update 901/src/pkg901/SpaceShip.java"
    This reverts commit 97953b510856b333571e77aa6ce48c95d3ba9a20.

--- 901/src/pkg901/SpaceShip.java
+++ 901/src/pkg901/SpaceShip.java
@@ -160,7 +160,6 @@ import java.util.ArrayList;
         g.drawImage(Tamperer,Window.getX(1240),Window.getY(50),
                 Window.getX(220),Window.getY(260),this);
 
-=======
 
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
