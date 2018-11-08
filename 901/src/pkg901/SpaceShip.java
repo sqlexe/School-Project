b5f568e0a02acd3d4fd5a4427320e00158c9d7a7
blockbuster20603 <44711763+blockbuster20603@users.noreply.github.com>
2018-11-08 11:49:10 -0800

    Update 901/src/pkg901/SpaceShip.java

--- 901/src/pkg901/SpaceShip.java
+++ 901/src/pkg901/SpaceShip.java
@@ -136,29 +136,10 @@ import java.util.ArrayList;
 
         g.drawImage(FeltBG,Window.getX(0),Window.getY(0),
                 Window.getWidth2(),Window.getHeight2(),this);
-<<<<<<< HEAD
-   
-        g.drawImage(MrYee,Window.getX(200),Window.getY(160),
-                Window.getX(220),Window.getY(260),this);
-        
-        g.drawImage(Alex,Window.getX(300),Window.getY(100),
-                Window.getX(220),Window.getY(260),this);
-        
-        g.drawImage(Tamperer,Window.getX(400),Window.getY(120),
-                Window.getX(220),Window.getY(260),this);
-=======
 
-
-
-        
-
-
-<<<<<<< HEAD
-       
-=======
 //        g.drawImage(Middle,Window.getX(80),Window.getY(80),
 //                Window.getX(160),Window.getY(160),this);
->>>>>>> d4ad065c8b6740010a45733d4ef9faf796522ac1
+
 
 //        g.drawImage(Middle,Window.getX(80),Window.getY(80),
 //                Window.getX(160),Window.getY(160),this);
@@ -171,20 +152,18 @@ import java.util.ArrayList;
         g.drawImage(MrYee,Window.getX(200),Window.getY(160),
                 Window.getX(220),Window.getY(260),this);
         
-<<<<<<< HEAD
         g.drawImage(Alex,Window.getX(600),Window.getY(100),
                 Window.getX(160),Window.getY(160),this);
         
         g.drawImage(Tamperer,Window.getX(1000),Window.getY(120),
                 Window.getX(160),Window.getY(160),this);
-=======
+
         g.drawImage(Alex,Window.getX(100),Window.getY(100),
                 Window.getX(220),Window.getY(260),this);
         
         g.drawImage(Tamperer,Window.getX(120),Window.getY(120),
                 Window.getX(220),Window.getY(260),this);
->>>>>>> d4ad065c8b6740010a45733d4ef9faf796522ac1
->>>>>>> a1f0f5004c735a81e20425e7b3ba5da7d630db9a
+
 
         g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
                 Window.getX(300),Window.getY(300),this);
