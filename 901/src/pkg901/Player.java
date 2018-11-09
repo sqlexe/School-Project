f4aa22d978fc576864a6813ac50c2608dee2ffe1
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-09 12:26:06 -0800

    Update 901/src/pkg901/Player.java

--- 901/src/pkg901/Player.java
+++ 901/src/pkg901/Player.java
@@ -1,7 +1,20 @@
 
 package pkg901;
+import java.io.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Player {
+    
+    public static void click(int xP, int yP)
+    {
+        
+    }
+    
     
 }
 
