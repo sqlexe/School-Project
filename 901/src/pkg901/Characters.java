5a51ceca9f5eb21adb1bc0aacf756e9e43856cff
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-09 11:43:46 -0800

    Update 901/src/pkg901/Characters.java

--- 901/src/pkg901/Characters.java
+++ 901/src/pkg901/Characters.java
@@ -1,12 +1,27 @@
 
 package pkg901;
-
+import java.io.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
 
 public class Characters {
+    private Image tamperer;
+    private Image alex;
+    private Image yee;
+    private Image student;
+    
     public static enum Character {
-        Mafia, Nurse, Detective, Innocent
+        Tamperer, Alex, Yee, Student
     }
     
+    public void Draw()
+    {
+        
+    }
     
     
 }
