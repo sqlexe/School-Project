b2196ecb6ae8be0fb4cac4fbd9023f768f37a389
Don't Bother <clj08222@gmail.com>
2018-11-13 15:53:52 -0800

    Update 901/src/pkg901/Room901.java

--- 901/src/pkg901/Room901.java
+++ 901/src/pkg901/Room901.java
@@ -164,31 +164,33 @@ import javax.sound.sampled.*;
             return;
         }
         //KEEP THESE THEYRE ARE RIGHT 
+        if(!homescreen)
+        {
+            g.drawImage(FeltBG,Window.getX(0),Window.getY(0),
+                    Window.getWidth2(),Window.getHeight2(),this);
 
-        g.drawImage(FeltBG,Window.getX(0),Window.getY(0),
-                Window.getWidth2(),Window.getHeight2(),this);
-        
 
-        
-        g.drawImage(MrYee,Window.getX(440),Window.getY(50),
-                Window.getX(220),Window.getY(260),this);
+            bgSound.stopPlaying = true;
+            g.drawImage(MrYee,Window.getX(440),Window.getY(50),
+                    Window.getX(220),Window.getY(260),this);
 
-       
-        g.drawImage(Alex,Window.getX(840),Window.getY(50),
-                Window.getX(220),Window.getY(260),this);
-        
-        g.drawImage(Tamperer,Window.getX(1240),Window.getY(50),
-                Window.getX(220),Window.getY(260),this);
 
-        g.drawImage(Back,Window.getX(1240),Window.getY(400),
-                   Window.getX(220),Window.getY(260),this);
-        
-        g.drawImage(Student,Window.getX(440),Window.getY(400),
-                Window.getX(220),Window.getY(260),this);
+            g.drawImage(Alex,Window.getX(840),Window.getY(50),
+                    Window.getX(220),Window.getY(260),this);
 
-        g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
-                Window.getX(300),Window.getY(300),this);
+            g.drawImage(Tamperer,Window.getX(1240),Window.getY(50),
+                    Window.getX(220),Window.getY(260),this);
 
+            g.drawImage(Back,Window.getX(1240),Window.getY(400),
+                       Window.getX(220),Window.getY(260),this);
+
+            g.drawImage(Student,Window.getX(440),Window.getY(400),
+                    Window.getX(220),Window.getY(260),this);
+
+            g.drawImage(Middle,Window.getX(Window.WINDOW_WIDTH/2-150),Window.getY(Window.WINDOW_HEIGHT/2-150),
+                    Window.getX(300),Window.getY(300),this);
+            
+        }
         g.setColor(Color.RED);
         g.drawRect(Window.getX(440),Window.getY(400),Window.getX(220),Window.getY(260));
         
@@ -391,6 +393,7 @@ class sound implements Runnable {
     Thread myThread;
     File soundFile;
     public boolean donePlaying = false;
+    public boolean stopPlaying = false;;
     sound(String _name)
     {
         soundFile = new File(_name);
@@ -416,7 +419,12 @@ class sound implements Runnable {
             }
         }
         donePlaying = true;
-
+        if(stopPlaying)
+        {
+            source.stop();
+            source.flush();
+            source.close();
+        }
         source.drain();
         source.close();
         }
