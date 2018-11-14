a8999cec0a60ea5e27f61c2ee357f621a4ce2b1e
Don't Bother <clj08222@gmail.com>
2018-11-14 12:41:13 -0800

    Update Room901.java

--- 901/src/pkg901/Room901.java
+++ 901/src/pkg901/Room901.java
@@ -392,48 +392,4 @@ class Drawing {
    
 
 }
-class sound implements Runnable {
-    Thread myThread;
-    File soundFile;
-    public boolean donePlaying = false;
-    public boolean stopPlaying = false;
-    sound(String _name)
-    {
-        soundFile = new File(_name);
-        myThread = new Thread(this);
-        myThread.start();
-    }
-    public void run()
-    {
-        try {
-        AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
-        AudioFormat format = ais.getFormat();
-    //    System.out.println("Format: " + format);
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-        SourceDataLine source = (SourceDataLine) AudioSystem.getLine(info);
-        source.open(format);
-        source.start();
-        int read = 0;
-        byte[] audioData = new byte[16384];
-        while (read > -1){
-            read = ais.read(audioData,0,audioData.length);
-            if (read >= 0) {
-                source.write(audioData,0,read);
-                if(stopPlaying)
-                    read = -1;
-            }
-        }        
-        
-        donePlaying = true;
 
-
-        source.drain();
-        source.close();
-        }
-        catch (Exception exc) {
-            System.out.println("error: " + exc.getMessage());
-            exc.printStackTrace();
-        }
-    }
-
-}
