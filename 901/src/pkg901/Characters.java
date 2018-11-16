fa3c56df2415bc7e6b5d87ae2f6169f113b38a4c
blackblackadder <44787656+blackblackadder@users.noreply.github.com>
2018-11-16 13:01:18 -0800

    Update Characters.java

--- 901/src/pkg901/Characters.java
+++ 901/src/pkg901/Characters.java
@@ -21,14 +21,17 @@ public class Characters {
         Tamperer, Alex, Yee, Student
     }
     
-    public  static void Draw(Room901 instance)
+    public  static void Draw(Room901 instance,boolean nightime,boolean gamescreen,boolean homeScreen )
     {
 
+        if (nightime && !gamescreen && !homeScreen)
+        {
         Draw.drawImage(MrYee, 440, 840, 0, 1, 1);
         Draw.drawImage(AlexI, 840, 840, 0, 1, 1);
         Draw.drawImage(TampererI, 1240, 250, 0, 1, 1);
         Draw.drawImage(Back, 1240, 840, 0, 1.1, 1.1);
         Draw.drawImage(StudentI, 440, 250, 0, 1, 1);
+        }
 //        g.drawImage(MrYee,Window.getX(440),Window.getY(50),
 //                Window.getX(220),Window.getY(260),instance);
 //       
