ad89e2aabcb288799345ab127871264d8cda2837
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-16 12:03:52 -0800

    Create Draw.java

---
+++ 901/src/pkg901/Draw.java
@@ -0,0 +1,49 @@
+
+package pkg901;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+class Draw {
+    
+    
+    private static Graphics2D g;
+    private static Room901 mainClassInst;
+
+    public static void setDrawingInfo(Graphics2D _g,Room901 _mainClassInst) {
+        g = _g;
+        mainClassInst = _mainClassInst;
+    }
+////////////////////////////////////////////////////////////////////////////
+    public static void drawCircle(int xpos,int ypos,double rot,double xscale,double yscale,Color color)
+    {
+        g.translate(xpos,ypos);
+        g.rotate(rot  * Math.PI/180.0);
+        g.scale( xscale , yscale );
+
+        g.setColor(color);
+        g.fillOval(-10,-10,20,20);
+
+        g.scale( 1.0/xscale,1.0/yscale );
+        g.rotate(-rot  * Math.PI/180.0);
+        g.translate(-xpos,-ypos);
+    }
+////////////////////////////////////////////////////////////////////////////
+    public static void drawImage(Image image,int xpos,int ypos,double rot,double xscale,
+            double yscale) {
+        int width = image.getWidth(mainClassInst);
+        int height = image.getHeight(mainClassInst);
+        g.translate(xpos,ypos);
+        g.rotate(rot  * Math.PI/180.0);
+        g.scale( xscale , yscale );
+
+        g.drawImage(image,-width/2,-height/2,
+        width,height,mainClassInst);
+
+        g.scale( 1.0/xscale,1.0/yscale );
+        g.rotate(-rot  * Math.PI/180.0);
+        g.translate(-xpos,-ypos);
+    }
+    
+}
