580f5cc43a6dca2a68aa7cbf8092149e936e3a0a
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-19 16:10:19 -0800

    Update Board.java

--- 901/src/pkg901/Board.java
+++ 901/src/pkg901/Board.java
@@ -3,8 +3,13 @@ package pkg901;
 
 public class Board {
     private static int day;
+    private static Player selectedPlayer;
     
     public static void click(int xP, int yP)
+    {
+        
+    }
+    public static void draw()
     {
         
     }
