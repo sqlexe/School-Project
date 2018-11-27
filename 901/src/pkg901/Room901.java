5b6868f25d740a828a57a6b4771eaa63ab7a90e0
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-27 12:25:07 -0800

    Update Room901.java

--- 901/src/pkg901/Room901.java
+++ 901/src/pkg901/Room901.java
@@ -67,50 +67,50 @@ import javax.sound.sampled.*;
                     
                     Board.click(e.getX() - Window.getX(0),
                         e.getY() - Window.getY(0));  
-               if(gamescreen )         {
-                    if(xpos>450 && xpos<555 && ypos>68 && ypos<97 )
-                    {
-                       kickp1++;
-                       Player.currentPlayer.advance();
-                     
-                  }
-                 if(xpos>450 && xpos<555 && ypos>128 && ypos<157 )
-                 {
-                     kickp2++;
-                     Player.currentPlayer.advance();
-                  }
+                    if(gamescreen ){
+                        if(xpos>450 && xpos<555 && ypos>68 && ypos<97 )
+                        {
+                           kickp1++;
+                           Player.currentPlayer.advance();
+
+                        }
+                        if(xpos>450 && xpos<555 && ypos>128 && ypos<157 )
+                        {
+                            kickp2++;
+                            Player.currentPlayer.advance();
+                        }
                         if(xpos>450 && xpos<555 && ypos>188 && ypos<217  ){
-                        kickp3++;
-                        Player.currentPlayer.advance();
-                  System.out.println(kickp3+"c");}
-                    if(xpos>450 && xpos<555 && ypos>248 && ypos<277 ){
-                        kickp4++;
-                        Player.currentPlayer.advance();
-                 System.out.println(kickp4+"d");}
-                   if(xpos>450 && xpos<555 && ypos>308 && ypos<337 ){
-                        kickp5++;
-                        Player.currentPlayer.advance();
-                  System.out.println(kickp5+"e");}
-                     if(xpos>450 && xpos<555 && ypos>368 && ypos<397 ){
-                        kickp6++;
-                        Player.currentPlayer.advance();
-                 System.out.println(kickp6+"f");}
-                   if(xpos>450 && xpos<555 && ypos>428 && ypos<457 ){
-                        kickp7++;
-                        Player.currentPlayer.advance();
-                 System.out.println(kickp7+"g");}
-               }
+                            kickp3++;
+                            Player.currentPlayer.advance();
+                            System.out.println(kickp3+"c");}
+                        if(xpos>450 && xpos<555 && ypos>248 && ypos<277 ){
+                            kickp4++;
+                            Player.currentPlayer.advance();
+                            System.out.println(kickp4+"d");}
+                       if(xpos>450 && xpos<555 && ypos>308 && ypos<337 ){
+                            kickp5++;
+                            Player.currentPlayer.advance();
+                            System.out.println(kickp5+"e");}
+                         if(xpos>450 && xpos<555 && ypos>368 && ypos<397 ){
+                            kickp6++;
+                            Player.currentPlayer.advance();
+                            System.out.println(kickp6+"f");}
+                       if(xpos>450 && xpos<555 && ypos>428 && ypos<457 ){
+                            kickp7++;
+                            Player.currentPlayer.advance();
+                            System.out.println(kickp7+"g");}
+                    }
                     
                     
                     
                     if(nightime)
-              {
-                  if(xpos>1490 && xpos<1485+265 && ypos>825 && ypos<920 )
-               {
-                   nightime=false;
-                   gamescreen=true;
-               }
-              }            
+                    {
+                        if(xpos>1490 && xpos<1485+265 && ypos>825 && ypos<920 )
+                        {
+                            nightime=false;
+                            gamescreen=true;
+                        }
+                    }            
                          
                     
                     
@@ -118,31 +118,30 @@ import javax.sound.sampled.*;
                     
                     
                     if(homescreen)    
-                 {
+                    {
      //+ code  
-                     if(xpos>xLeft1 && xpos <xLeft2 && ypos>365 && ypos<470)
-                        if(numPlayers <7)
-                        numPlayers++;
+                        if(xpos>xLeft1 && xpos <xLeft2 && ypos>365 && ypos<470)
+                            if(numPlayers <7)
+                                numPlayers++;
                      
 // - code
-                      if(xpos>950)
-                        if(xpos<1020)
-                        if(ypos>650&&ypos<750)
-                        if(numPlayers>3)
-                            numPlayers--;
+                        if(xpos>950)
+                          if(xpos<1020)
+                              if(ypos>650&&ypos<750)
+                                  if(numPlayers>3)
+                                      numPlayers--;
 
- // 
-                 }
+                    }
                     
 
-                                                    System.out.println(Player.currentPlayer+"");
+                    System.out.println(Player.currentPlayer+"");
                        
                        
                         
                         
                         
                         
-                        if(!homescreen)
+                    if(!homescreen)
 
                     Board.click(e.getX(),
                         e.getY());  
