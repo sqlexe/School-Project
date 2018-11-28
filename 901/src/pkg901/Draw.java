5c3dd267ee880e6351bd88ba225345af116ae53a
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-28 15:17:23 -0800

    Update Draw.java

--- 901/src/pkg901/Draw.java
+++ 901/src/pkg901/Draw.java
@@ -1,4 +1,3 @@
-
 package pkg901;
 
 import java.awt.Color;
@@ -6,46 +5,46 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 
 class Draw {
-    
-    
+
     private static Graphics2D g;
     private static Room901 mainClassInst;
 
-    public static void setDrawingInfo(Graphics2D _g,Room901 _mainClassInst) {
+    public static void setDrawingInfo(Graphics2D _g, Room901 _mainClassInst) {
 //        System.out.println("setDrawingInfo " + g);
         g = _g;
         mainClassInst = _mainClassInst;
     }
 ////////////////////////////////////////////////////////////////////////////
-    public static void drawCircle(int xpos,int ypos,double rot,double xscale,double yscale,Color color)
-    {
-        g.translate(xpos,ypos);
-        g.rotate(rot  * Math.PI/180.0);
-        g.scale( xscale , yscale );
+
+    public static void drawCircle(int xpos, int ypos, double rot, double xscale, double yscale, Color color) {
+        g.translate(xpos, ypos);
+        g.rotate(rot * Math.PI / 180.0);
+        g.scale(xscale, yscale);
 
         g.setColor(color);
-        g.fillOval(-10,-10,20,20);
+        g.fillOval(-10, -10, 20, 20);
 
-        g.scale( 1.0/xscale,1.0/yscale );
-        g.rotate(-rot  * Math.PI/180.0);
-        g.translate(-xpos,-ypos);
+        g.scale(1.0 / xscale, 1.0 / yscale);
+        g.rotate(-rot * Math.PI / 180.0);
+        g.translate(-xpos, -ypos);
     }
 ////////////////////////////////////////////////////////////////////////////
-    public static void drawImage(Image image,int xpos,int ypos,double rot,double xscale,
+
+    public static void drawImage(Image image, int xpos, int ypos, double rot, double xscale,
             double yscale) {
         int width = image.getWidth(mainClassInst);
         int height = image.getHeight(mainClassInst);
 //        System.out.println(g);
-        g.translate(xpos,ypos);
-        g.rotate(rot  * Math.PI/180.0);
-        g.scale( xscale , yscale );
+        g.translate(xpos, ypos);
+        g.rotate(rot * Math.PI / 180.0);
+        g.scale(xscale, yscale);
 
-        g.drawImage(image,-width/2,-height/2,
-        width,height,mainClassInst);
+        g.drawImage(image, -width / 2, -height / 2,
+                width, height, mainClassInst);
 
-        g.scale( 1.0/xscale,1.0/yscale );
-        g.rotate(-rot  * Math.PI/180.0);
-        g.translate(-xpos,-ypos);
+        g.scale(1.0 / xscale, 1.0 / yscale);
+        g.rotate(-rot * Math.PI / 180.0);
+        g.translate(-xpos, -ypos);
     }
-    
+
 }
