431d6698b79e0f9f24d2fe071205fb0fff3a4fea
KodingKiddo <44794479+KodingKiddo@users.noreply.github.com>
2018-11-28 15:17:19 -0800

    Update Characters.java

--- 901/src/pkg901/Characters.java
+++ 901/src/pkg901/Characters.java
@@ -1,5 +1,5 @@
-
 package pkg901;
+
 import java.io.*;
 import java.awt.*;
 import java.awt.geom.*;
@@ -9,21 +9,20 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Characters {
+
     private static Image MrYee = Toolkit.getDefaultToolkit().getImage("./Mr.Yee.jpg");
     private static Image AlexI = Toolkit.getDefaultToolkit().getImage("./Alex.jpg");
     private static Image StudentI = Toolkit.getDefaultToolkit().getImage("./Student.jpg");
-    private static Image TampererI = Toolkit.getDefaultToolkit().getImage("./Tamperer.jpg");;
+    private static Image TampererI = Toolkit.getDefaultToolkit().getImage("./Tamperer.jpg");
+    ;
     private static Image Back = Toolkit.getDefaultToolkit().getImage("./Blank.jpg");
     private static Graphics2D g;
-    
-    
+
     public static enum Character {
         Tamperer, Alex, Yee, Student
     }
-    
-    public  static void Draw(Room901 instance,boolean nightime,boolean gamescreen,boolean homeScreen )
-    {
 
+    public static void Draw(Room901 instance, boolean nightime, boolean gamescreen, boolean homeScreen) {
 
 //        if (nightime && !gamescreen && !homeScreen)
 //        {
@@ -33,13 +32,11 @@ public class Characters {
 //        Draw.drawImage(Back, 1240, 840, 0, 1.1, 1.1);
 //        Draw.drawImage(StudentI, 440, 250, 0, 1, 1);
 //        }
-
 //        Draw.drawImage(MrYee, 440, 840, 0, .8, .8);
 //        Draw.drawImage(AlexI, 840, 840, 0, .8, .8);
 //        Draw.drawImage(TampererI, 1240, 250, 0, .8, .8);
 //        Draw.drawImage(Back, 1240, 840, 0, .9, .9);
 //        Draw.drawImage(StudentI, 440, 250, 0, .8, .8);
-
 //        g.drawImage(MrYee,Window.getX(440),Window.getY(50),
 //                Window.getX(220),Window.getY(260),instance);
 //       
@@ -54,67 +51,65 @@ public class Characters {
 //        
 //        g.drawImage(StudentI,Window.getX(440),Window.getY(400),
 //                Window.getX(220),Window.getY(260),instance);
-
     }
-    public static void Assign(Player players[]){
+
+    public static void Assign(Player players[]) {
         boolean keepLooping = true;
         //alex
-        while(keepLooping)
-        {
-            int pick = (int)(Math.random()*(players.length));
-            if(players[pick].character==null)
-            {
+        while (keepLooping) {
+            int pick = (int) (Math.random() * (players.length));
+            if (players[pick].character == null) {
                 players[pick].setRole(Character.Alex);
-                keepLooping=false;
+                keepLooping = false;
             }
         }
-        
+
         //Yee
         keepLooping = true;
-        while(keepLooping)
-        {
-            int pick = (int)(Math.random()*(players.length));
-            if(players[pick].character==null)
-            {
+        while (keepLooping) {
+            int pick = (int) (Math.random() * (players.length));
+            if (players[pick].character == null) {
                 players[pick].setRole(Character.Yee);
-                keepLooping=false;
+                keepLooping = false;
             }
         }
-        
+
         //Tamperer
         keepLooping = true;
-        while(keepLooping)
-        {
-            int pick = (int)(Math.random()*(players.length));
-            if(players[pick].character==null)
-            {
+        while (keepLooping) {
+            int pick = (int) (Math.random() * (players.length));
+            if (players[pick].character == null) {
                 players[pick].setRole(Character.Tamperer);
-                keepLooping=false;
+                keepLooping = false;
             }
         }
-        
-        for(int i=0; i<players.length;i++)
-        {
-            if(players[i].character==null)
+
+        for (int i = 0; i < players.length; i++) {
+            if (players[i].character == null) {
                 players[i].setRole(Character.Student);
+            }
         }
-        
-            
+
     }
-    
-    
- public  static void DrawYee(int xpos, int ypos, double rot, double xscale, double yscale){
-         Draw.drawImage(MrYee, xpos, ypos, rot, xscale, yscale);}
-       public  static void DrawAlex(int xpos, int ypos, double rot, double xscale, double yscale){
-    Draw.drawImage(AlexI, xpos, ypos, rot, xscale, yscale);}
-        public  static void DrawTampererI(int xpos, int ypos, double rot, double xscale, double yscale){
-       Draw.drawImage(TampererI, xpos, ypos, rot, xscale, yscale);}
-       public  static void DrawBack(int xpos, int ypos, double rot, double xscale, double yscale){
-        Draw.drawImage(Back, xpos, ypos, rot, xscale, yscale);}
-        public  static void DrawStudentI(int xpos, int ypos, double rot, double xscale, double yscale){
-       Draw.drawImage(StudentI, xpos, ypos, rot, xscale, yscale);}
 
+    public static void DrawYee(int xpos, int ypos, double rot, double xscale, double yscale) {
+        Draw.drawImage(MrYee, xpos, ypos, rot, xscale, yscale);
+    }
 
+    public static void DrawAlex(int xpos, int ypos, double rot, double xscale, double yscale) {
+        Draw.drawImage(AlexI, xpos, ypos, rot, xscale, yscale);
+    }
 
+    public static void DrawTampererI(int xpos, int ypos, double rot, double xscale, double yscale) {
+        Draw.drawImage(TampererI, xpos, ypos, rot, xscale, yscale);
+    }
+
+    public static void DrawBack(int xpos, int ypos, double rot, double xscale, double yscale) {
+        Draw.drawImage(Back, xpos, ypos, rot, xscale, yscale);
+    }
+
+    public static void DrawStudentI(int xpos, int ypos, double rot, double xscale, double yscale) {
+        Draw.drawImage(StudentI, xpos, ypos, rot, xscale, yscale);
+    }
 
 }
